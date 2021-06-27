c015f29e9d6701a96f5925155f957c076b2b9050
Chetan Lodha <chetan.lodha23@gmail.com>
2021-06-27 15:33:28 +0530

    Animate items on page load

--- js/script.js
+++ js/script.js
@@ -21,6 +21,60 @@ window.onload = () => {
     onResize();
   }
   document.querySelectorAll('.nav-elements li').forEach(node => node.addEventListener('click', () => collapseNavBar()))
+  setTimeout(() => {
+    document.querySelector('.PortfolioItemsContainer').innerHTML = `
+      <div class="PortfolioItem" style="--order: 1">
+          <a href="https://drive.google.com/drive/folders/1paStYMrNJu-yhWok5ormK3XqnUP3ue1Q?usp=sharing"
+              target="_blank" rel="noreferrer">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Sharehere</span>
+                  <span class="PortfolioItemTextSubHeading">jQuery</span>
+              </div>
+          </a>
+      </div>
+      <div class="PortfolioItem" style="--order: 2">
+          <a href="https://chetan-notes.netlify.app" target="_blank" rel="noreferrer">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Quick Notes</span>
+                  <span class="PortfolioItemTextSubHeading">MERN</span>
+              </div>
+          </a>
+      </div>
+      <div class="PortfolioItem" style="--order: 3">
+          <a href="https://chetan-todo.netlify.app" target="_blank" rel="noreferrer">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Todo List</span>
+                  <span class="PortfolioItemTextSubHeading">React-Redux</span>
+              </div>
+          </a>
+      </div>
+      <div class="PortfolioItem" style="--order: 4">
+          <a href="#">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Portfolio</span>
+                  <span class="PortfolioItemTextSubHeading">HTML-CSS</span>
+              </div>
+          </a>
+      </div>
+      <div class="PortfolioItem" style="--order: 5">
+          <a href="https://play.google.com/store/apps/details?id=substratum.theme.ice" target="_blank"
+              rel="noreferrer">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Ice Substratum Theme</span>
+                  <span class="PortfolioItemTextSubHeading">Android</span>
+              </div>
+          </a>
+      </div>
+      <div class="PortfolioItem" style="--order: 6">
+          <a href="https://play.google.com/store/apps/details?id=substratum.xperia.lockscreens"
+              target="_blank" rel="noreferrer">
+              <div class="PortfolioItemSpanContainer">
+                  <span class="PortfolioItemTextHeading">Xperia Lockscreen Clock</span>
+                  <span class="PortfolioItemTextSubHeading">Android</span>
+              </div>
+          </a>
+      </div>`;
+  }, 2000)
 }
 
 function collapseNavBar() {
